c41a048d80b4bec6d72eec1d1eb2f728d3284ae6
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-09-01 00:46:31 +0400

    .

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def display_score():
     score_surf = font.render(f"Score:  {current_time}", False, (64, 64, 64))
     score_rect = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rect)
+    return current_time
 
 
 pygame.init()
@@ -19,6 +20,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 font = pygame.font.Font("font/Pixeltype.ttf", 50)
+score = 0
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -33,6 +35,12 @@ player_gravity = 0
 player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
+
+game_name = font.render('Pixel Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
+
+game_message = font.render('Press space to play again...', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center=(400, 340))
 
 while True:
     for event in pygame.event.get():
@@ -55,7 +63,7 @@ while True:
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 4
 
@@ -75,6 +83,16 @@ while True:
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
+
+        score_message = font.render(f"Your Score: {score}", False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center=(400, 330))
+
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
 
     pygame.display.update()
     clock.tick(60)
