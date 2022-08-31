bc0fc7ee846ca5724e09162574ec58e53875edb2
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-09-01 00:37:39 +0400

    .

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from sys import exit
 
 width = 800
 height = 400
-game_active = True
+game_active = False
 start_time = 0
+
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
@@ -19,13 +20,8 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-
-
-# score_surf = font.render("Runner", False, (64, 64, 64))
-# score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(600, 300))
@@ -33,6 +29,10 @@ snail_rect = snail_surf.get_rect(bottomright=(600, 300))
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 player_gravity = 0
+
+player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400, 200))
 
 while True:
     for event in pygame.event.get():
@@ -50,24 +50,21 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
-                start_time = int(pygame.time.get_ticks()/1000)
-
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
         display_score()
 
-
         snail_rect.x -= 4
 
-        if snail_rect.right <= 0 :
+        if snail_rect.right <= 0:
             snail_rect.left = 800
 
         screen.blit(snail_surf, snail_rect)
 
-
-        player_gravity +=1
+        player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300:
             player_rect.bottom = 300
@@ -76,9 +73,8 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill('Yellow')
-
-
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
 
     pygame.display.update()
     clock.tick(60)
