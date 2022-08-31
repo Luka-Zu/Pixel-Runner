66e82d3e352736d93879fbf8b63e60b9906d5849
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-09-01 02:03:26 +0400

    .

--- main.py
+++ main.py
@@ -126,6 +126,9 @@ while True:
 
         obstacles_rect_list.clear()
 
+        player_rect.midbottom = (80, 300)
+        player_gravity = 0
+
         score_message = font.render(f"Your Score: {score}", False, (111, 196, 169))
         score_message_rect = score_message.get_rect(center=(400, 330))
 
