f3cea31bebe41e84770196bb4cdb5971cd4de2ea
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-31 18:01:41 +0400

    .

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import pygame
 from sys import exit
 
 width = 800
-heigth = 400
+height = 400
 
 pygame.init()
-screen = pygame.display.set_mode((width, heigth))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = font.render("Runner", False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+
+score_surf = font.render("Runner", False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(400, 50))
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright=(600, 300))
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -25,13 +31,25 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
 
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
+    screen.blit(score_surf, score_rect)
 
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    snail_rect.x -= 4
+
+    if snail_rect.right <= 0 :
+        snail_rect.left = 800
+
+    screen.blit(snail_surf, snail_rect)
+
+    screen.blit(player_surf, player_rect)
+
+
+
+    mouse_pos = pygame.mouse.get_pos()
+
+
 
     pygame.display.update()
     clock.tick(60)
